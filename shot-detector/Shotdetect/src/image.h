634701db448957afc7ba3941aef4eb5e06c523b3
Yu Mao <ymao1@andrew.cmu.edu>
2017-04-10 18:57:55 -0400

    add shot-detector

---
+++ shot-detector/Shotdetect/src/image.h
@@ -0,0 +1,64 @@
+/*
+ * $Id: image.h 194 2010-09-30 23:35:11Z peterb $
+ * $Date: 2010-10-01 01:35:11 +0200 (Fri, 01 Oct 2010) $
+ */
+
+#ifndef __IMAGE_H__
+#define __IMAGE_H__
+
+#ifndef INT64_C
+#define INT64_C(c) (c##LL)
+#define UINT64_C(c) (c##ULL)
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
+
+#include <string.h>
+#include <string>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "src/film.h"
+
+#include <gd.h>
+#ifdef WXWIDGETS
+#include <wx/wx.h>
+#endif
+
+#define BEGIN 0x00
+#define END 0x01
+#define MIDDLE 0x02
+using namespace std;
+
+class conf;
+class film;
+
+class image {
+  film *f;
+  bool thumb_set;
+  bool shot_set;
+
+ public:
+  int width;
+  int height;
+  int width_thumb;
+  int height_thumb;
+  string thumb;
+  string img;
+  int id;
+  short type;  // BEGIN || END || MIDDLE
+  int SaveFrame(AVFrame *pFrame, int frame_number);
+  int create_img_dir();
+  image(film *, int, int, int, short, bool, bool);
+};
+
+#endif /* !__IMAGE_H__ */
